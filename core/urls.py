d4f838308747cec43d553f34286bb56dd44e0f2a
luchov27 <dreyes12@utmachala.edu.ec>
2025-11-06 01:01:26 -0500

    guardado

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.inicio, name='inicio'),
-    path('home-02/', views.home_02, name='home_02'),
     path('login/', views.login_usuario, name='login'),
     path('logout/', views.logout_usuario, name='logout'),
     path('dashboard/', views.dashboard_redirect, name='dashboard'),
